98c20c4eaae692565e686721522b2231b1e3a47f
alexzw7 <alexzw@umich.edu>
2022-12-07 21:19:21 -0500

    added files

---
+++ knn.py
@@ -0,0 +1,83 @@
+import numpy as np 
+import cv2
+import glob
+import os
+import matplotlib.pyplot as plt
+import time
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+dim = 100
+def getYourFruits(fruits, data_type, print_n=False, k_fold=False):
+    images = []
+    labels = []
+    val = ['Training', 'Test']
+    if not k_fold:
+        path = "./Fruit-Images-Dataset/" + data_type + "/"
+        for i,f in enumerate(fruits):
+            p = path + f
+            j=0
+            for image_path in glob.glob(os.path.join(p, "*.jpg")):
+                image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+                image = cv2.resize(image, (dim, dim))
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                images.append(image)
+                labels.append(i)
+                j+=1
+            if(print_n):
+                print("There are " , j , " " , data_type.upper(), " images of " , fruits[i].upper())
+        images = np.array(images)
+        labels = np.array(labels)
+        return images, labels
+    else:
+        for v in val:
+            path = "./Fruit-Images-Dataset/" + v + "/"
+            for i,f in enumerate(fruits):
+                p = path + f
+                j=0
+                for image_path in glob.glob(os.path.join(p, "*.jpg")):
+                    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+                    image = cv2.resize(image, (dim, dim))
+                    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                    images.append(image)
+                    labels.append(i)
+                    j+=1
+        images = np.array(images)
+        labels = np.array(labels)
+        return images, labels
+    
+def getAllFruits():
+    fruits = []
+    for fruit_path in glob.glob("./Fruit-Images-Dataset/Training/*"):
+        fruit = fruit_path.split("/")[-1]
+        fruits.append(fruit)
+    return fruits
+
+#Choose your Fruits
+#fruits = ['Pineapple', 'Banana', 'Blueberry', 'Orange', 'Cocos', 'Mango', 'Peach', 'Pear', 'Watermelon', 'Strawberry', 'Avocado', 'Corn']
+rfruits = getAllFruits()[0:25]
+
+#Get Images and Labels 
+X_t, y_train =  getYourFruits(rfruits, 'Training', print_n=True, k_fold=False)
+X_test, y_test = getYourFruits(rfruits, 'Test', print_n=True, k_fold=False)
+
+print(len(y_train))
+
+#Scale Data Images
+scaler = StandardScaler()
+X_train = scaler.fit_transform([i.flatten() for i in X_t])
+X_test = scaler.fit_transform([i.flatten() for i in X_test])
+
+knn = KNeighborsClassifier(n_neighbors=2)
+knn.fit(X_train, y_train)
+start = time.time()
+y_pred = knn.predict(X_test)
+end = time.time()
+
+#Evaluation
+precision = metrics.accuracy_score(y_pred, y_test) * 100
+print("Accuracy with K-NN: {0:.2f}%".format(precision))
+print("Time: ", end - start)
+#cm , _ = plot_confusion_matrix(y_test, y_pred, classes=y_train, normalize=True, title='Normalized confusion matrix')
+#plt.show()
